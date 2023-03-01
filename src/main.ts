f3d50610996139ed35708d10ceea37448595c426
Yagiz Degirmenci <yagizcanilbey1903@gmail.com>
2023-03-01 16:01:36 +0300

    feat: update '@core/actions' to 'v1.10'
    Signed-off-by: Yagiz Degirmenci <yagizcanilbey1903@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,16 @@ import { exec } from 'child_process';
 
 const AWS_DEFAULT_OUTPUT = 'json';
 const AWS_PAGER = '';
-const AWS_ACCESS_KEY_ID: string = core.getInput('aws-access-key-id', {required: true})
-const AWS_SECRET_ACCESS_KEY: string = core.getInput('aws-secret-access-key', {required: true})
+const AWS_ACCESS_KEY_ID: string = core.getInput('aws-access-key-id', { required: true })
+const AWS_SECRET_ACCESS_KEY: string = core.getInput('aws-secret-access-key', { required: true })
 const AWS_DEFAULT_REGION: string = core.getInput('aws-default-region')
-const image: string  = core.getInput('image', {required: true}) 
-const tags: string[] = core.getInput('tags').split(',').map((tag) =>tag.trim()) || ['latest'];
+const image: string = core.getInput('image', { required: true })
+const tags: string[] = core.getInput('tags').split(',').map((tag) => tag.trim()) || ['latest'];
 let distributedImages: string[] = [];
 
 async function run(): Promise<void> {
   try {
-    const {stdout, stderr} = await execute(`aws sts get-caller-identity --output json --no-cli-pager --region ${AWS_DEFAULT_REGION}`);
+    const { stdout, stderr } = await execute(`aws sts get-caller-identity --output json --no-cli-pager --region ${AWS_DEFAULT_REGION}`);
     core.debug(`Account data: ${stdout}`);
     core.debug(`Stderr: ${stderr}`);
     const accountData: AccountData = JSON.parse(stdout);
@@ -27,7 +27,7 @@ async function run(): Promise<void> {
   } catch (error) {
     if (error instanceof Error) {
       core.setFailed(error.message)
-    }   
+    }
   }
 }
 
@@ -39,23 +39,24 @@ interface CommandOutput {
 export async function execute(command: string): Promise<CommandOutput> {
   return new Promise<CommandOutput>((resolve, reject) => {
     exec(command, {
-    shell: '/bin/bash',
-    encoding: 'utf-8',
-    env: {
-    ...process.env,
-    AWS_ACCESS_KEY_ID,
-    AWS_SECRET_ACCESS_KEY,
-    AWS_DEFAULT_REGION,
-    AWS_DEFAULT_OUTPUT,
-    AWS_PAGER,
-  }},
-         (error: Error|null, stdout: string, stderr: string) => {
-           if (error) {
-             reject(error);
-           } else {
-             resolve({stdout, stderr});
-           }
-         });
+      shell: '/bin/bash',
+      encoding: 'utf-8',
+      env: {
+        ...process.env,
+        AWS_ACCESS_KEY_ID,
+        AWS_SECRET_ACCESS_KEY,
+        AWS_DEFAULT_REGION,
+        AWS_DEFAULT_OUTPUT,
+        AWS_PAGER,
+      }
+    },
+      (error: Error | null, stdout: string, stderr: string) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve({ stdout, stderr });
+        }
+      });
   });
 }
 
@@ -67,7 +68,7 @@ interface AccountData {
 
 // dockerPush pushes the image to the ECR registry.
 async function dockerPush(image: string, tag: string, accountData: AccountData): Promise<void> {
-  const repository: string = `${accountData.Account}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com`; 
+  const repository: string = `${accountData.Account}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com`;
   const uri: string = `${repository}/${image}:${tag}`;
   core.debug(`Pushing image ${image} as ${uri}`);
   await execute(`docker tag ${image} ${uri}`);
